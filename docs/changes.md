a147afc4549d4bb58ad2d9040a6cfe426c28e44b
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2018-05-22 19:05:02 +0900

    #2: initial attempt to side navigation

---
+++ docs/changes.md
@@ -0,0 +1,42 @@
+## Change Log
+
+#### 2.0 Beta3 (To be released)
+
+* Added hawtio BOM
+
+#### 2.0 Beta2
+
+* Keycloak integration support
+* Bug fixes
+
+#### 2.0 Beta1
+
+* hawtio Maven plugin
+* Improved Spring Boot support
+* Log plugin
+* Diagnostics plugin
+* Karaf RBAC support
+* PatternFly-based login page and logo
+* Upgrade to Servlet API 3.1
+* Port fixes from hawtio v1
+* Bug fixes
+
+#### 2.0 M3
+
+* Switch from bower to yarn
+* Help plugin
+* Port fixes from hawtio v1
+
+#### 2.0 M2
+
+* Migrate to PatternFly
+* Bug fixes
+* Port fixes from hawtio v1
+
+#### 2.0 M1
+
+* Initial 2.0 milestone release
+
+#### 1.x
+
+See: https://github.com/hawtio/hawtio/blob/master/CHANGES.md
--- src/templates/docs-template.js
+++ src/templates/docs-template.js
@@ -1,15 +1,58 @@
 import React from 'react'
-import { PageHeader } from 'react-bootstrap';
+import {
+  Grid,
+  Row,
+  Col,
+  Nav,
+  NavItem,
+  PageHeader
+} from 'react-bootstrap';
 
 const DocsTemplate = ({ data }) => {
   const { frontmatter, html } = data.markdownRemark
   return (
-    <div className="container">
-      <PageHeader>
-        <h1>{frontmatter.title}</h1>
-      </PageHeader>
-      <div dangerouslySetInnerHTML={{ __html: html }} />
-    </div>
+    <Grid>
+      <Row>
+        <Col md={3}>
+          <h4 style={{ marginTop: 50 }}>Using Hawtio</h4>
+          <Nav bsStyle="pills" stacked>
+            <NavItem eventKey={1} href="/docs/get-started/">
+              Get Started
+            </NavItem>
+            <NavItem eventKey={2} href="/docs/configuration/">
+              Configuration
+            </NavItem>
+            <NavItem eventKey={3} href="/docs/plugins/">
+              Plugins
+            </NavItem>
+            <NavItem eventKey={4} href="/docs/maven-plugins/">
+              Maven Plugins
+            </NavItem>
+            <NavItem eventKey={5} href="/docs/articles/">
+              Articles
+            </NavItem>
+          </Nav>
+          <h4>General Docs</h4>
+          <Nav bsStyle="pills" stacked>
+            <NavItem eventKey={1} href="/docs/faq/">
+              FAQ
+            </NavItem>
+            <NavItem eventKey={2} href="/docs/changes/">
+              Change Log
+            </NavItem>
+            <NavItem eventKey={3} href="/docs/contributing/">
+              Contributing
+            </NavItem>
+          </Nav>
+        </Col>
+        <Col md={9}>
+          <PageHeader>
+            <h1>{frontmatter.title}</h1>
+          </PageHeader>
+          <div dangerouslySetInnerHTML={{ __html: html }} />
+        </Col>
+      </Row>
+    </Grid>
   )
 }
 
