b559536f7127dddd7384cc153bdea685613b04c0
Alexandre Kieling <alex.kieling@gmail.com>
2019-10-28 11:52:28 -0300

    chore: migrate to Gatsby v2

---
+++ docs/styles-d3b567486fc5068fc23b.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{q4sD:function(n,o,w){},xTT7:function(n,o,w){}}]);
+//# sourceMappingURL=styles-d3b567486fc5068fc23b.js.map
--- src/templates/docs-template.js
+++ src/templates/docs-template.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link, graphql, withPrefix } from 'gatsby'
 import {
   Grid,
   Row,
@@ -7,10 +8,7 @@ import {
   NavItem as RBNavItem,
   PageHeader
 } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
-
-import { withPrefix } from 'gatsby-link'
-
+import Layout from '../components/layout'
 import '../css/docs.css'
 
 const NavItem = props =>
@@ -21,60 +19,62 @@ const NavItem = props =>
 const DocsTemplate = ({ data }) => {
   const { frontmatter, html } = data.markdownRemark
   return (
-    <Grid>
-      <Row>
-        <Col xsHidden smHidden md={2} style={{ position: 'fixed' }}>
-          <h5 style={{ marginTop: 50, marginBottom: 30 }}>
-            <Link to={withPrefix('/docs/')}>Index</Link>
-          </h5>
-          <h5>Using Hawtio</h5>
-          <Nav stacked>
-            <NavItem href={withPrefix('/docs/get-started/')}>
-              Get Started
-            </NavItem>
-            <NavItem href={withPrefix('/docs/configuration/')}>
-              Configuration
-            </NavItem>
-            <NavItem href={withPrefix('/docs/plugins/')}>
-              Plugins
-            </NavItem>
-            <NavItem href={withPrefix('/docs/maven-plugins/')}>
-              Maven Plugins
-            </NavItem>
-          </Nav>
-          <h5>General Docs</h5>
-          <Nav stacked>
-            <NavItem href={withPrefix('/docs/faq/')}>
-              FAQ
-            </NavItem>
-            <NavItem href='https://github.com/hawtio/hawtio/blob/master/CHANGES.md'>
-              Change Log
-            </NavItem>
-            <NavItem href={withPrefix('/docs/contributing/')}>
-              Contributing
-            </NavItem>
-          </Nav>
-          <h5>Developers</h5>
-          <Nav stacked>
-            <NavItem href='https://github.com/hawtio/hawtio/blob/master/DEVELOPERS.md'>
-              Developer Guide
-            </NavItem>
-            <NavItem href='https://github.com/hawtio/hawtio/blob/master/BUILDING.md'>
-              Building
-            </NavItem>
-            <NavItem href='https://github.com/hawtio/hawtio/blob/master/RELEASING.md'>
-              Release Guide
-            </NavItem>
-          </Nav>
-        </Col>
-        <Col md={10} mdOffset={2}>
-          <PageHeader>
-            <h1>{frontmatter.title}</h1>
-          </PageHeader>
-          <div dangerouslySetInnerHTML={{ __html: html }} />
-        </Col>
-      </Row>
-    </Grid>
+    <Layout>
+      <Grid>
+        <Row>
+          <Col xsHidden smHidden md={2} style={{ position: 'fixed' }}>
+            <h5 style={{ marginTop: 50, marginBottom: 30 }}>
+              <Link to={withPrefix('/docs/')}>Index</Link>
+            </h5>
+            <h5>Using Hawtio</h5>
+            <Nav stacked>
+              <NavItem href={withPrefix('/docs/get-started/')}>
+                Get Started
+              </NavItem>
+              <NavItem href={withPrefix('/docs/configuration/')}>
+                Configuration
+              </NavItem>
+              <NavItem href={withPrefix('/docs/plugins/')}>
+                Plugins
+              </NavItem>
+              <NavItem href={withPrefix('/docs/maven-plugins/')}>
+                Maven Plugins
+              </NavItem>
+            </Nav>
+            <h5>General Docs</h5>
+            <Nav stacked>
+              <NavItem href={withPrefix('/docs/faq/')}>
+                FAQ
+              </NavItem>
+              <NavItem href='https://github.com/hawtio/hawtio/blob/master/CHANGES.md'>
+                Change Log
+              </NavItem>
+              <NavItem href={withPrefix('/docs/contributing/')}>
+                Contributing
+              </NavItem>
+            </Nav>
+            <h5>Developers</h5>
+            <Nav stacked>
+              <NavItem href='https://github.com/hawtio/hawtio/blob/master/DEVELOPERS.md'>
+                Developer Guide
+              </NavItem>
+              <NavItem href='https://github.com/hawtio/hawtio/blob/master/BUILDING.md'>
+                Building
+              </NavItem>
+              <NavItem href='https://github.com/hawtio/hawtio/blob/master/RELEASING.md'>
+                Release Guide
+              </NavItem>
+            </Nav>
+          </Col>
+          <Col md={10} mdOffset={2}>
+            <PageHeader>
+              <h1>{frontmatter.title}</h1>
+            </PageHeader>
+            <div dangerouslySetInnerHTML={{ __html: html }} />
+          </Col>
+        </Row>
+      </Grid>
+    </Layout>
   )
 }
 
